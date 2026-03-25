7b31ae64034883bd6715e79de1cee1e49ee21e89
alanbishop75 <alan_bishop@yahoo.com>
2026-03-25 10:26:24 +0000

    Fix logo filename case for production

--- app/components/GolfAILogo.tsx
+++ app/components/GolfAILogo.tsx
@@ -2,7 +2,7 @@ export default function GolfAILogo() {
   return (
     <div className="w-[180px] max-w-[92vw] md:w-[380px]">
       <img
-        src="/MyAIGolfCoachLogo.png"
+        src="/MyAIGolfCoachLogo.PNG"
         alt="MyAIGolfCoach Logo"
         className="h-auto w-full object-contain"
       />
